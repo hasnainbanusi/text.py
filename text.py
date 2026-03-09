35a6a31d415c61e1d2a6302e45298a78bbb089bd
Hasnain Banusi <your-github-hasnain10.official@gmail.com>
2026-03-09 22:34:56 +0500

    text.py

--- text.py
+++ text.py
@@ -1,5 +1,9 @@
+def hello():
+    """
+    this is demo file
+    """
+
+
 print("This is file is dedicated to my app meant for networking like linkedin in pakistan")
 
-
-
-this is my new addition to this repo for the first time in github which will be reflected on my local machine also.
+hello()
